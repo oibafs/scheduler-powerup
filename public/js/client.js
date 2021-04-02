d87aacb18287582fab5220390314239193c4110e
oibafs <fabioscaravelli@gmail.com>
2021-04-02 19:38:36 -0300

    First deploy of REST API

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,47 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
+var restApiCardButtonCallback = function(t) {
+  return t.getRestApi()
+   .isAuthorized()
+   .then(function(authorized) {
+     if (!authorized) {
+       // You might be tempted to call client.authorize from a capability handler like the one we are in right now.
+       // Unfortunately this does not register as a click by the browser, and it will block the popup. Instead, we need to
+       // open a t.popup from our capability handler, and load an iframe that contains a button that calls client.authorize.
+       return t.popup({
+         title: 'Authorize Trello\'s REST API',   
+         url: './api-client-authorize.html',
+       })
+     } else {
+       return t.popup({
+         title: "Choose an action",
+         items: [{
+           // We'll use the client on the authorization page to make an example request.
+           text: 'Postpone card',
+           callback: function(t) {
+             return t.card('all')
+              .then(function(card) {
+                console.log(JSON.stringify(card, null, 2));
+              })
+           }
+         }, {
+           // You can de-authorize the REST API client with a call to .clearToken()
+           text: 'Unauthorize',
+           callback: function(t) {
+             return t.getRestApi()
+               .clearToken()
+               .then(function() {
+                 t.alert('You\'ve successfully deauthorized!'); 
+                 t.closePopup(); 
+               })
+           }
+         }]
+       })
+     }
+   });
+ }
+
 TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
 	// 'card-buttons': function(t, options) {
@@ -21,4 +62,10 @@ TrelloPowerUp.initialize({
 	// 	  }];
 	// })
 	// },
-});
+  'card-buttons': function(t, options) {
+    return [{
+      icon: BLACK_ROCKET_ICON,
+      text: 'Scheduler',
+      callback: restApiCardButtonCallback,
+    }];
+  },});
