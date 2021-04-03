f075f6cc80c99ef5d8480e04ab4ede57ad4181ad
oibafs <fabioscaravelli@gmail.com>
2021-04-03 16:20:03 -0300

    Returning to initial module tentative

--- public/js/equalize.js
+++ public/js/equalize.js
@@ -1,6 +1,6 @@
 import { getCustomFields } from './common.js';
 
-window.equalize = function equalize(card, customFieldModel) {
+export function equalize(card, customFieldModel) {
   let work = {};
   work.start = card.start;
   work.customFields = card.customFieldItems ? getCustomFields(card.customFieldItems, customFieldModel) : {};
