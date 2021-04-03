480f171fa7d068eed624700a680c8b5112cbd833
oibafs <fabioscaravelli@gmail.com>
2021-04-03 18:32:13 -0300

    Further tests on authorization

--- public/js/client.js
+++ public/js/client.js
@@ -82,8 +82,25 @@ function authorize() {
 
 function equalize(id) {
 
-  const authorized = authorize();
-  console.log(authorized);
+  if (!window.Trello.authorized()) {
+
+    window.Trello.authorize({
+      type: 'popup',
+      name: 'Getting Started Application',
+      scope: {
+        read: 'true',
+        write: 'true' },
+      expiration: 'never',
+      success: authenticationSuccess,
+      error: authenticationFailure
+    });    
+
+  } else {
+    console.log('Already authorized!');
+  }
+
+  // const authorized = authorize();
+  // console.log(authorized);
     // .then(() => {
 
     //   window.Trello.cards.get(id, {
