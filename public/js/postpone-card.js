62e2a070b1a7efb1f795e04d434b2ef47a772d06
oibafs <fabioscaravelli@gmail.com>
2021-04-06 16:59:58 -0300

    Use proxy method to edit custom field

--- public/js/postpone-card.js
+++ public/js/postpone-card.js
@@ -255,7 +255,7 @@ const postponeCard = (id, token) => {
       // Update custom fields
       for (let i = 0; i < output.customFields.length; i ++) {
 
-        fetch(`https://api.trello.com/1/cards/${card.id}/checkItem/${output.customFields[i].id}?key=039f30a96f8f3e440addc095dd42f87d&token=${token}`, {
+        fetch(`https://scheduler-ruby.vercel.app/api/1/trello/cards/${card.id}/checkItem/${output.customFields[i].id}?key=039f30a96f8f3e440addc095dd42f87d&token=${token}`, {
           method: 'POST',
           body: JSON.stringify(output.customFields[i].body),
           headers: {
