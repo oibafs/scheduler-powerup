f023a38251a15b18f8f038f1050bbf9a5dc162bc
oibafs <fabioscaravelli@gmail.com>
2021-04-10 13:28:43 -0300

    Sort list - Implementation test

--- public/js/client.js
+++ public/js/client.js
@@ -87,7 +87,35 @@ TrelloPowerUp.initialize({
       text: 'Scheduler',
       callback: restApiCardButtonCallback,
     }];
-  }
+  },
+  'list-sorters': function (t) {
+    return t.list('name', 'id')
+    .then(function (list) {
+      return [{
+        text: "Priority",
+        callback: function (t, opts) {
+          // Trello will call this if the user clicks on this sort
+          // opts.cards contains all card objects in the list
+          var sortedCards = opts.cards.sort(
+            function(a,b) {
+              console.log(a);
+              console.log(b);
+              return 0;
+/*               if (a.name > b.name) {
+                return 1;
+              } else if (b.name > a.name) {
+                return -1;
+              }
+              return 0; */
+            });
+
+          return {
+            sortedIds: sortedCards.map(function (c) { return c.id; })
+          };
+        }
+      }];
+    });
+  }  
 }, {
   // appKey: 'your_key_here',
   appKey: '039f30a96f8f3e440addc095dd42f87d',
