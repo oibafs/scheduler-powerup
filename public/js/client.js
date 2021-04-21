83f2c1df9c91865b027d7c463346682e1a1ead70
oibafs <fabioscaravelli@gmail.com>
2021-04-21 18:42:48 -0300

    Importance implementation

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 /* global TrelloPowerUp */
 var Promise = TrelloPowerUp.Promise;
 
@@ -201,6 +203,133 @@ const onTodayClick = (t, opts) => {
 
 }
 
+const onImportanceClick = (t, opts) => {
+
+  t.board("customFields", "labels")
+    .then((board) => {
+      const lblStar = board.labels.filter(i => i.name === "star")[0];
+
+      t.lists("id", "name")
+        .then((lists) => {
+          let doneList = lists.filter(i => i.name === "Done")[0];
+          doneList = doneList ? doneList.id : "";
+
+          t.cards("id", "idList", "customFieldItems", "labels")
+            .then((cards) => {
+
+              const cardsImportance = cards.map((item) => {
+                const priority = fieldValue(board.customFields, item.customFieldItems, "Priority");
+                const star = item.labels ? item.labels.filter(i => i.name === "star").length > 0 : false;
+                const due = item.due ? new Date(item.due) : new Date("2999/12/31");
+                const category = fieldValue(board.customFields, item.customFieldItems, "Category");
+                let nextAction = fieldValue(board.customFields, item.customFieldItems, "Next action");
+                const importance = fieldValue(board.customFields, item.customFieldItems, "Importance");
+                let newImportance = 2;
+
+                switch (priority) {
+                  case "Urgent":
+                    newImportance += 3;
+                    break;
+
+                  case "Important":
+                    newImportance += 2;
+                    break;
+
+                  case "Medium":
+                    newImportance += 1;
+                    break;
+
+                  default:
+                    break;
+                }
+
+                newImportance += star ? 1 : 0;
+
+                const today = new Date();
+
+                // Calculate difference between two dates in days
+                const dateDiff = (originalDate, futureDate) => {
+                  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+                  return Math.floor((futureDate - originalDate) / _MS_PER_DAY);
+                }
+
+                const daysToDue = dateDiff(today, due);
+
+                if (daysToDue < 0) {
+                  newImportance += 6;
+                } else if (daysToDue < 1) {
+                  newImportance += 5;
+                } else if (daysToDue < 2) {
+                  newImportance += 3;
+                } else if (daysToDue < 7) {
+                  newImportance += 2;
+                } else if (daysToDue < 14) {
+                  newImportance += 1;
+                }
+
+                newImportance += (category === "Work") ? 1 : 0;
+
+                nextAction = (nextAction === "null") ? new Date("2999/12/31") : nextAction;
+
+                if (nextAction < 0) {
+                  newImportance += 2;
+                } else if (nextAction < 1) {
+                  newImportance += 1;
+                }
+
+                return {
+                  id: item.id,
+                  importance: importance,
+                  newImportance: newImportance,
+                }
+
+              })
+
+              const cardsToChange = cardsImportance.filter(item => item.newImportance != item.importance);
+
+              if (cardsToChange.length > 0) {
+
+                cardsToChange.map((item) => {
+                  console.log(cardsToChange);
+                })
+
+                /*
+                          t.getRestApi()
+                            .getToken()
+                            .then((token) => {
+                
+                              if (token) {
+                                window.Trello.setToken(token);
+                
+                                cardsToChange.map((item) => {
+                
+                                  if (item.addLabel) {
+                                    console.log("POST");
+                                    window.Trello.post(`cards/${item.id}/idLabels/?value=${lblToday.id}`, null, todayResponse, todayResponse);
+                                  } else if (item.deleteLabel) {
+                                    console.log("DELETE");
+                                    window.Trello.delete(`cards/${item.id}/idLabels/${lblToday.id}`, null, todayResponse, todayResponse);
+                                  };
+                
+                                });
+                
+                              } else {
+                                t.alert("Not authorized!");
+                              }
+                
+                            })
+                */
+
+              }
+
+            })
+
+        })
+
+    })
+
+}
+
 const getBadges = (t) => {
 
   return t.board("customFields")
@@ -279,16 +408,27 @@ TrelloPowerUp.initialize({
       });
   },
   'board-buttons': function (t, opts) {
-    return [{
-      // we can either provide a button that has a callback function
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
+    return [
+      {
+        // we can either provide a button that has a callback function
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'Today',
+        callback: onTodayClick,
+        condition: 'edit'
       },
-      text: 'Today',
-      callback: onTodayClick,
-      condition: 'edit'
-    }];
+      {
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'Importance',
+        callback: onImportanceClick,
+        condition: 'edit'
+      },
+    ];
   },
   'card-badges': (t, opts) => {
     return getBadges(t);
